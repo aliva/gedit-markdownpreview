26feb6ddf29e9b93115543870ba288c7f5d604d8
Ali Vakilzade <ali@vakilzade.com>
2019-02-02 23:28:13 +0330

    Update markdownpreview.py

--- markdownpreview.py
+++ markdownpreview.py
@@ -20,24 +20,29 @@ class MarkdownPreviewViewActivatable(GObject.Object, Gedit.ViewActivatable):
         GObject.Object.__init__(self)
 
     def do_activate(self):
-        print("Plugin created for", self.view)
-
         self.scrolledwindow = self.view.get_parent()
         self.webview = None
+        self.signal_handlers = [None, None, None]
 
         buffer = self.view.get_buffer()
         buffer.connect('notify::language', self.language_changed)
+        self.language_changed(buffer, None)
 
     def language_changed(self, buffer, language):
-        adjustment = self.scrolledwindow.get_vadjustment()
+        language = buffer.get_language()
 
-        if buffer.get_language().get_id() == 'markdown':
+        if language is None:
+            return
+
+        lang_id = language.get_id()
+
+        if self.webview is None and lang_id == 'markdown':
+            self.webview = WebKit2.WebView()
+
             buffer.connect('changed', self.changed)
-
+            adjustment = self.scrolledwindow.get_vadjustment()
             adjustment.connect('changed', self.scrolled)
             adjustment.connect('value-changed', self.scrolled)
-
-            self.webview = WebKit2.WebView()
 
             box = self.scrolledwindow.get_parent()
             box.pack_end(self.webview, True, True, 0)
@@ -45,11 +50,7 @@ class MarkdownPreviewViewActivatable(GObject.Object, Gedit.ViewActivatable):
             self.webview.show_all()
             self.changed(buffer)
         elif self.webview:
-            self.webview.destroy()
-            buffer.disconnect_by_func(self.changed)
-            adjustment.disconnect_by_func(self.scrolled)
-            adjustment.disconnect_by_func(self.scrolled)
-            self.webview = None
+            self.do_deactivate()
 
     def changed(self, buffer):
         text = buffer.get_text(
@@ -70,7 +71,6 @@ class MarkdownPreviewViewActivatable(GObject.Object, Gedit.ViewActivatable):
         value = self.get_scroller_pos()
         text = text + "<script>" + scrolljs.format(value=value) + "</script>"
         self.webview.load_html(text)
-        print(text)
         self.scrolled(None)
 
     def get_scroller_pos(self):
@@ -83,8 +83,15 @@ class MarkdownPreviewViewActivatable(GObject.Object, Gedit.ViewActivatable):
         self.webview.run_javascript(scrolljs.format(value=value))
 
     def do_deactivate(self):
-        print("Plugin stopped for", self.view)
+        self.webview.destroy()
+        self.webview = None
+
+        buffer = self.view.get_buffer()
+        buffer.disconnect_by_func(self.changed)
+
+        adjustment = self.scrolledwindow.get_vadjustment()
+        adjustment.disconnect_by_func(self.scrolled)
+        adjustment.disconnect_by_func(self.scrolled)
 
     def do_update_state(self):
-        # Called whenever the view has been updated
-        print("Plugin update for", self.view)
+        pass
